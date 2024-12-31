d36355f5ff826e23f77ed807844dda028cf1331e
Joseph456D <82628259+Joseph456D@users.noreply.github.com>
2025-01-01 00:25:42 +0530

    Update code

--- Time_calculator.c
+++ Time_calculator.c
@@ -2,38 +2,43 @@
 #include <stdlib.h>
 void main()
 {
-    int temp1, temp2, choice, choice2, time2_hh, time2_mm, time2_ss, time1_hh = 0, time1_mm = 0, time1_ss = 0;
+    int temp1, temp2, choice, choice2 = 1, time2_hh, time2_mm, time2_ss, time1_hh = 0, time1_mm = 0, time1_ss = 0;
     printf("Enter the time in HH MM SS Format : ");
-Time_A:
-    scanf("%d%d%d", &time1_hh, &time1_mm, &time1_ss);
-    if (time1_mm >= 60)
+    while (1)
     {
-        printf("Wrong Input\nType Again : ");
-        goto Time_A;
-    }
-    if (time1_ss >= 60)
-    {
-        printf("Wrong Input\nType Again : ");
-        goto Time_A;
+        scanf("%d%d%d", &time1_hh, &time1_mm, &time1_ss);
+        if (time1_mm >= 60 || time1_ss >= 60)
+        {
+            printf("Wrong Input\nType Again : ");
+        }
+        else
+            break;
     }
     do
     {
-    Choice:
         printf("Press \e[1m\"1\"\e[m for adding and \e[1m\"2\"\e[m for subtracting : ");
-        scanf("%d", &choice2);
+        while (1)
+        {
+            scanf("%d", &choice2);
+            if (choice2 != 1 && choice2 != 2)
+            {
+                printf("Wrong choice Press Correct Option : ");
+            }
+            else
+                break;
+        }
         printf("Enter next time in HH MM SS Format : ");
-    Time_B:
-        scanf("%d%d%d", &time2_hh, &time2_mm, &time2_ss);
-        if (time2_mm >= 60)
+        while (1)
         {
-            printf("Wrong Input\nType Again : ");
-            goto Time_B;
+            scanf("%d%d%d", &time2_hh, &time2_mm, &time2_ss);
+            if (time2_mm >= 60 || time2_ss >= 60)
+            {
+                printf("Wrong Input\nType Again : ");
+            }
+            else
+                break;
         }
-        if (time2_ss >= 60)
-        {
-            printf("Wrong Input\nType Again : ");
-            goto Time_B;
-        }
+
         if (choice2 == 1)
         {
             time1_hh += time2_hh;
@@ -56,29 +61,22 @@ Time_A:
         }
         else if (choice2 == 2)
         {
-            
+
             time1_hh -= time2_hh;
             time1_mm -= time2_mm;
             time1_ss -= time2_ss;
-            
+
             if (time1_ss < 0)
             {
-                time1_ss *= -1;
-                time1_ss = 60 - time1_ss;
+                time1_ss += 60;
                 time1_mm--;
             }
-            
+
             if (time1_mm < 0)
             {
-                time1_mm *= -1;
-                time1_mm = 60 - time1_mm;
+                time1_mm += 60;
                 time1_hh--;
             }
-        }
-        else
-        {
-            printf("Wrong choice Press Correct Option : ");
-            goto Choice;
         }
 
         printf("The resultant time is %02d:%02d:%02d\n", time1_hh, time1_mm, time1_ss);
@@ -86,8 +84,9 @@ Time_A:
         printf("Press \e[1m\"1\"\e[m to continue and \e[1m\"0\"\e[m to stop : ");
         scanf("%d", &choice);
     } while (choice);
-    printf("Stopping...");
+    printf("Stopping...\n");
 }
+
 /*
         \e[1m -- Bold
         \e[m --Reset Bold
